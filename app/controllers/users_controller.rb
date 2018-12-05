1ce1fe298b0adc226e589aada219a36358320c5e
murfmk <murfmk1130@gmail.com>
2018-12-05 15:05:18 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login @user
       flash[:notice] = "ユーザー登録が完了しました"
-      redirect_to root_url
+      redirect_to @user
     else
       render "new"
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
