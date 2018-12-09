be3c6d6015dac5e472e914b5695c97aa19fc826c
murfmk <murfmk1130@gmail.com>
2018-12-09 20:05:03 +0900

    create ranking-page

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,5 +1,3 @@
 class RanksController < ApplicationController
-  def index
-    @misroposts = Micropost.find(Comment.group(:micropost_id).order(‘count(micropost_id) desk’)limit(5).pluck(:micropost_id))
-  end
+  
 end
