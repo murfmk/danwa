d4f02492713ff3e8a34ec7f106cb6e36f3ea8fbc
murfmk <murfmk1130@gmail.com>
2018-12-11 11:29:27 +0900

    like model destroy

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.4.2'
 gem 'bootstrap-sass'
 gem 'carrierwave'
 gem 'bcrypt'
+gem 'font-awesome-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_012042) do
+ActiveRecord::Schema.define(version: 2018_12_11_012832) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -26,10 +26,10 @@ ActiveRecord::Schema.define(version: 2018_12_11_012042) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "tweet_id"
+    t.integer "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+    t.index ["comment_id"], name: "index_likes_on_comment_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2018_12_11_012042) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
