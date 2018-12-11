adfe48b1c2b8aacf8dd61a39eb1f65f6e9e5c065
murfmk <murfmk1130@gmail.com>
2018-12-11 10:22:41 +0900

    rails g like model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   has_many :comments
   mount_uploader :picture, PictureUploader
+  # default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
 end
