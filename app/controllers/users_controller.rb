ceb4ac2cbc6cbd14514fa843b02f780813dbd613
murfmk <murfmk1130@gmail.com>
2018-12-21 00:35:04 +0900

    layout update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :option, :update, :destroy, :follow, :unfollow]
+  before_action :logged_in_user, only: [:show, :edit, :option, :update, :destroy, :follow, :unfollow]
   before_action :correct_user,   only: [:edit, :update, :destroy]
   def new
     @user = User.new
