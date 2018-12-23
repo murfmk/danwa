12fdd4bc36bb27731548be616d5b9ca80dac467f
murfmk <murfmk1130@gmail.com>
2018-12-24 02:30:39 +0900

    テーマ削除時のエラー解除(micropost.rbにmount_uploaderがあった)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  mount_uploader :picture, PictureUploader
   # default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
